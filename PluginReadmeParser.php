0c396bf5146b9df11f4c290622862a24d2e270d3
Claes Ö <claes@stenbergit.se>
2019-01-23 17:45:19 +0100

    Add widget to parse readme.md files into a div tag.

--- PluginReadmeParser.php
+++ PluginReadmeParser.php
@@ -3,9 +3,26 @@
  * https://github.com/erusev/parsedown
  */
 class PluginReadmeParser{
+  /**
+   */
   public function parse_text($text){
     include_once __DIR__.'/lib/Parsedown.php';
     $Parsedown = new Parsedown();
     return $Parsedown->text($text);
+  }
+  public function widget_readme($data){
+    $data = new PluginWfArray($data);
+    $file = $data->get('data/file');
+    $file = wfGlobals::getAppDir().$file;
+    $exist = wfFilesystem::fileExist($file);
+    $readme = null;
+    if($exist){
+      $readme = file_get_contents($file);
+      wfPlugin::includeonce('readme/parser');
+      $parser = new PluginReadmeParser();
+      $readme = $parser->parse_text($readme);
+    }
+    $element = wfDocument::createHtmlElement('div', $readme);
+    wfDocument::renderElement(array($element));
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Buto-Plugin-ReadmeParser
 Parse README.md files.
+
+
+## Widget
+
+Parse content from readme.md file and put into a div.
+Add path to file from application dir to a readme.md file.
+Example: /sys/mercury/README.md
+
+
+```
+type: widget
+data:
+  plugin: readme/parser
+  method: readme
+  data:
+    file: /sys/mercury/README.md
+```
+
+## PHP
+Parse readme.md content.
+```
+wfPlugin::includeonce('readme/parser');
+$parser = new PluginReadmeParser();
+$html = $parser->parse_text('Readme.md content.');
+```
