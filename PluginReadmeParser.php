bf6ca3e9d2241d514e432cf174e9ee6d50ae6e01
Claes Ö <>
2021-12-01 18:36:49 +0100

    1.2.0

--- PluginReadmeParser.php
+++ PluginReadmeParser.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * https://github.com/erusev/parsedown
- */
 class PluginReadmeParser{
-  /**
-   */
   public function parse_text($text){
     include_once __DIR__.'/lib/Parsedown.php';
     $Parsedown = new Parsedown();
@@ -12,17 +7,30 @@ class PluginReadmeParser{
   }
   public function widget_readme($data){
     $data = new PluginWfArray($data);
-    $file = $data->get('data/file');
-    $file = wfGlobals::getAppDir().$file;
-    $exist = wfFilesystem::fileExist($file);
-    $readme = null;
-    if($exist){
-      $readme = file_get_contents($file);
-      wfPlugin::includeonce('readme/parser');
-      $parser = new PluginReadmeParser();
-      $readme = $parser->parse_text($readme);
+    wfPlugin::includeonce('readme/parser');
+    $parser = new PluginReadmeParser();
+    if($data->get('data/file')){
+      $file = $data->get('data/file');
+      $file = wfGlobals::getAppDir().$file;
+      $exist = wfFilesystem::fileExist($file);
+      $readme = null;
+      if($exist){
+        $readme = file_get_contents($file);
+        $readme = $parser->parse_text($readme);
+      }
+      $element = wfDocument::createHtmlElement('div', $readme);
+      wfDocument::renderElement(array($element));
     }
-    $element = wfDocument::createHtmlElement('div', $readme);
-    wfDocument::renderElement(array($element));
+    if($data->get('data/content')){
+      $content = wfSettings::getSettingsFromYmlString($data->get('data/content'));
+      $content = str_replace('[h1]', '#', $content);
+      $content = str_replace('[h2]', '##', $content);
+      $content = str_replace('[h3]', '###', $content);
+      $content = str_replace('[h4]', '####', $content);
+      $content = str_replace('[h5]', '#####', $content);
+      $content = $parser->parse_text($content);
+      $element = wfDocument::createHtmlElement('div', $content);
+      wfDocument::renderElement(array($element));
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,21 +1,47 @@
 # Buto-Plugin-ReadmeParser
-Parse README.md files.
-
+Parse README.md files or content.
 
 ## Widget
-
 Parse content from readme.md file and put into a div.
 Add path to file from application dir to a readme.md file.
 Example: /sys/mercury/README.md
-
-
 ```
 type: widget
 data:
   plugin: readme/parser
   method: readme
   data:
+```
+
+### File
+```
     file: /sys/mercury/README.md
+```
+
+### Content
+Plain text.
+```
+    content: 'Some text...'
+```
+Get content from yml file.
+```
+    content: yml:/_path_to_yml_/data.yml:my_param/text
+```
+
+#### Special settings
+Special settings to avoid yml comments started with #.
+```
+[h1] Headline
+[h2] Headline
+[h3] Headline
+[h4] Headline
+[h5] Headline
+```
+
+#### Standard formation
+```
+**Bold**
+*Italic*
 ```
 
 ## PHP
@@ -25,3 +51,6 @@ wfPlugin::includeonce('readme/parser');
 $parser = new PluginReadmeParser();
 $html = $parser->parse_text('Readme.md content.');
 ```
+
+## Github
+https://github.com/erusev/parsedown
